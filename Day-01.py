6e013547a61eebd7ffedbda3f307354114a2656d
Bharathi S <bharathigoudabharathi@gmail.com>
2026-09-22 20:21:10 +0530

    Add Week 2 Day 1-If Else Conditions

--- Day-01.py
+++ Day-01.py
@@ -1,6 +1,30 @@
-'''print("Hello World!")
-print("I am starting my AI Engineering journey.")
-print("I am learning Python.")'''
+'''#1.Check weather a number is positive or negative.
+number = int(input("Enter a number:"))
+if number >= 0:
+    print("The number is positive.")
+else:
+    print("The number is negative.")
 
-name = "Bharathi"
-print("My name is", name)
+#2.Check whether a number is even or odd
+number=int(input("Enter a number:"))
+if number%2==0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+#3.Check Whether a person is eligible to vote
+age=int(input("Enter your age:"))
+if age>=18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")'''
+
+#4.Check Whether a student is passed or failed
+marks=int(input("Enter your marks:"))
+if marks>=45:
+    print("Student has passed.")
+else:
+    print("Student has failed.")
+
+
+
